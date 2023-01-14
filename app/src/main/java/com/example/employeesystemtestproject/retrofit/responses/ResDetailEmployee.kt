76e2d22609059baca8ada115a61a73faecc5af41
ESimonLeon <54182074+ESimonLeon@users.noreply.github.com>
2023-01-13 21:46:35 -0600

    Se agrega Login

---
+++ app/src/main/java/com/example/employeesystemtestproject/retrofit/responses/ResDetailEmployee.kt
@@ -0,0 +1,9 @@
+package com.example.employeesystemtestproject.retrofit.responses
+
+import com.example.employeesystemtestproject.retrofit.model.DataEmployee
+
+class ResDetailEmployee {
+    var status: String? = null
+    var data: DataEmployee? = null
+    var message: String? = null
+}
